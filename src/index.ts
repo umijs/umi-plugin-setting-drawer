48bd9bc674bb74f083b53a16ad8ab38733380819
xiefengnian.xfn <xiefengnian.xfn@antgroup.com>
2021-11-03 14:30:54 +0800

    feat: mfsu supports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import { IApi, utils } from 'umi';
 import { join } from 'path';
 import getLayoutContent from './getLayoutContent';
+import { readFileSync } from 'fs';
 
 const DIR_NAME = 'plugin-setting-drawer';
 
 export default (api: IApi) => {
   api.onGenerateFiles(() => {
     api.writeTmpFile({
-      path: join(DIR_NAME, 'SettingDrawer.tsx'),
-      content: getLayoutContent(
-        utils.winPath(join(__dirname, './layout/index.js')),
+      // compiled path: lib/index.js
+      path: join(DIR_NAME, 'layout.tsx'),
+      content: readFileSync(
+        join(__dirname, '../src/layout/index.tsx'),
+        'utf-8',
       ),
+    });
+    api.writeTmpFile({
+      path: join(DIR_NAME, 'SettingDrawer.tsx'),
+      content: getLayoutContent('./layout.tsx'),
     });
   });
   api.modifyRoutes(routes => {
