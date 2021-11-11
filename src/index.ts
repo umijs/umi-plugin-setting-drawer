c71409be0ba58298180fe9a97d007ad3c2ced64c
chenshuai2144 <qixian.cs@outlook.com>
2021-11-11 22:40:40 +0800

    fix defualtSetting error
    close https://github.com/ant-design/ant-design-pro/issues/9095

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,69 @@
 import { IApi, utils } from 'umi';
 import { join } from 'path';
 import getLayoutContent from './getLayoutContent';
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 
 const DIR_NAME = 'plugin-setting-drawer';
 
+const layoutTsx = `// @ts-nocheck
+import React from "react";
+// @ts-ignore
+import { useModel } from "umi";
+import { SettingDrawer } from "@ant-design/pro-layout";
+import defaultSettings from "../../../config/defaultSettings";
+
+const BasicLayout = (props: any) => {
+  const { children } = props;
+  const initialInfo = useModel ? useModel("@@initialState") : undefined;
+  if (!initialInfo) {
+    return children;
+  }
+  delete defaultSettings.logo;
+  delete defaultSettings.pwa;
+  delete defaultSettings.colorWeak;
+  delete defaultSettings.iconfontUrl;
+  
+  const { initialState, setInitialState } = initialInfo;
+  const settingsConfig =
+    initialState?.settings && Object.keys(initialState?.settings).length > 0
+      ? initialState?.settings
+      : defaultSettings;
+  return (
+    <>
+      {children}
+      <SettingDrawer
+        {...initialState?.SettingDrawer}
+        settings={settingsConfig}
+        onSettingChange={(settings) => {
+          setInitialState({ ...initialState, settings });
+        }}
+      />
+    </>
+  );
+};
+
+export default BasicLayout;`;
+
 export default (api: IApi) => {
   api.onGenerateFiles(() => {
+    const layoutPath = join(DIR_NAME, 'SettingDrawer.tsx');
+    const defaultSettingPath = join(
+      api.paths.cwd || '',
+      'config',
+      'defaultSettings',
+    );
+    // 如果defaultSettings.ts存在，读取一下作为默认值
+    if (
+      existsSync(`${defaultSettingPath}.js`) ||
+      existsSync(`${defaultSettingPath}.ts`)
+    ) {
+      try {
+        writeFileSync(join(__dirname, '../src/layout/index.tsx'), layoutTsx);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
     api.writeTmpFile({
       // compiled path: lib/index.js
       path: join(DIR_NAME, 'layout.tsx'),
@@ -15,8 +72,9 @@ export default (api: IApi) => {
         'utf-8',
       ),
     });
+
     api.writeTmpFile({
-      path: join(DIR_NAME, 'SettingDrawer.tsx'),
+      path: layoutPath,
       content: getLayoutContent('./layout.tsx'),
     });
   });
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+// @ts-nocheck
+import React from "react";
 // @ts-ignore
-import { useModel } from 'umi';
-import { SettingDrawer } from '@ant-design/pro-layout';
+import { useModel } from "umi";
+import { SettingDrawer } from "@ant-design/pro-layout";
+import defaultSettings from "../../../config/defaultSettings";
 
 const BasicLayout = (props: any) => {
   const { children } = props;
-  const initialInfo = useModel ? useModel('@@initialState') : undefined;
+  const initialInfo = useModel ? useModel("@@initialState") : undefined;
   if (!initialInfo) {
     return children;
   }
+  delete defaultSettings.logo;
+  delete defaultSettings.pwa;
+  delete defaultSettings.colorWeak;
+  delete defaultSettings.iconfontUrl;
+  
   const { initialState, setInitialState } = initialInfo;
+  const settingsConfig =
+    initialState?.settings && Object.keys(initialState?.settings).length > 0
+      ? initialState?.settings
+      : defaultSettings;
   return (
     <>
       {children}
       <SettingDrawer
-        {...initialState?.settingDrawer}
-        settings={initialState?.settings}
-        onSettingChange={settings => {
+        {...initialState?.SettingDrawer}
+        settings={settingsConfig}
+        onSettingChange={(settings) => {
           setInitialState({ ...initialState, settings });
         }}
       />
